2b36402a9e6a9dc57c43f190d0646420cee9fb1e
AzunyanTaichou <meitanteinep@gmail.com>
2020-01-15 13:39:02 +0800

    Add IInteractable interface

---
+++ Assets/Scripts/Interactables/IInteractable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    Vector3 position { get; }
+    void DisplayInteractability(); // Handles highlighting objects when nearby or displaying an icon to display interactability of an object
+    void Interact(); // Handles interaction
+}
