54673c38bdebcec28f5d1518f643668af0267186
AzunyanTaichou <meitanteinep@gmail.com>
2020-01-15 13:41:51 +0800

    Add Interactor script

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Interactor : MonoBehaviour
+{
+    private IInteractable[] interactableTargets;
+    private GameObject currentTarget;
+
+    public IInteractable[] InteractableTargets { get => interactableTargets; set => interactableTargets = value; }
+    public GameObject CurrentTarget { get => currentTarget; set => currentTarget = value; }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        InteractableTargets = collider.gameObject.GetComponents<IInteractable>();
+        InteractableTargets = null;
+        CurrentTarget = null;
+        //Debug.Log("No longer interacting with " + collider.name);
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        InteractableTargets = collider.gameObject.GetComponents<IInteractable>();
+
+        if (InteractableTargets.Length == 0) return;
+        foreach (var targets in InteractableTargets) targets.DisplayInteractability();
+        currentTarget = collider.gameObject;
+        //Debug.Log(currentTarget);
+    }
+}
