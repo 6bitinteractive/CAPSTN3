82486025f1e0374153a84a9d226ece98b752ae1f
AzunyanTaichou <meitanteinep@gmail.com>
2020-01-15 13:41:23 +0800

    Add Biteable script

---
+++ Assets/Scripts/Interactables/Biteable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SphereCollider))]
+
+public class Biteable : MonoBehaviour, IInteractable
+{  
+    public Vector3 position =>  transform.position;
+    public UnityEvent OnBite;
+
+    public void Interact()
+    {
+        Debug.Log("Biting " + gameObject.name);
+        OnBite.Invoke();
+    }
+
+    public void DisplayInteractability()
+    {
+      
+    }
+}
