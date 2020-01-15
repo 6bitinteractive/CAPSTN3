c0ac9627ba37dcbd30dd84c1f169ed5bcdc25f10
AzunyanTaichou <meitanteinep@gmail.com>
2020-01-15 13:41:02 +0800

    Add Bark script

---
+++ Assets/Scripts/Bite.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bite : MonoBehaviour
+{
+    public void BiteEvent(Biteable target)
+    {
+        target.Interact();
+    }
+}
