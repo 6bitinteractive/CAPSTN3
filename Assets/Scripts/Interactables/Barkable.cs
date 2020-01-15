13ba15312512d86ac1be1855e3a8ed91c07900df
AzunyanTaichou <meitanteinep@gmail.com>
2020-01-15 13:41:34 +0800

    Add Barkable script

---
+++ Assets/Scripts/Interactables/Barkable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SphereCollider))]
+
+public class Barkable : MonoBehaviour, IInteractable
+{
+    public Vector3 position => transform.position;
+    public UnityEvent OnBark;
+
+    public void Interact()
+    {
+        Debug.Log("Woof");
+        OnBark.Invoke();
+    }
+
+    public void DisplayInteractability()
+    {
+       
+    }
+}
