95082f36c9dec60302db784c770e478346996519
Meitantei-NepNep <nparuli@gmail.com>
2020-01-20 18:15:37 +0800

    Adjust Interactor script
    Prevent player from being able to interact with multiple objects at the same time

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -7,25 +7,32 @@ public class Interactor : MonoBehaviour
 {
     private IInteractable[] interactableTargets;
     private GameObject currentTarget;
+    private bool canInteract = true;
 
     public IInteractable[] InteractableTargets { get => interactableTargets; set => interactableTargets = value; }
     public GameObject CurrentTarget { get => currentTarget; set => currentTarget = value; }
+    public bool CanInteract { get => canInteract; set => canInteract = value; }
 
     private void OnTriggerExit(Collider collider)
     {
-        InteractableTargets = collider.gameObject.GetComponents<IInteractable>();
-        InteractableTargets = null;
-        CurrentTarget = null;
+        if (canInteract)
+        {
+            InteractableTargets = collider.gameObject.GetComponents<IInteractable>();
+            InteractableTargets = null;
+            CurrentTarget = null;
+        }    
         //Debug.Log("No longer interacting with " + collider.name);
     }
 
     private void OnTriggerStay(Collider collider)
     {
-        InteractableTargets = collider.gameObject.GetComponents<IInteractable>();
-
-        if (InteractableTargets.Length == 0) return;
-        foreach (var targets in InteractableTargets) targets.DisplayInteractability();
-        currentTarget = collider.gameObject;
+        if (canInteract)
+        {
+            InteractableTargets = collider.gameObject.GetComponents<IInteractable>();
+            if (InteractableTargets.Length == 0) return;
+            foreach (var targets in InteractableTargets) targets.DisplayInteractability();
+            currentTarget = collider.gameObject;
+        }
         //Debug.Log(currentTarget);
     }
 }
