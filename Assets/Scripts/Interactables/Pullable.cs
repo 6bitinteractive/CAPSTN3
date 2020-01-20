151652d2d5997ece683639c009b0c7aea1413de6
Meitantei-NepNep <nparuli@gmail.com>
2020-01-20 18:14:31 +0800

    Add Pullable script

---
+++ Assets/Scripts/Interactables/Pullable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FixedJoint))]
+[RequireComponent(typeof(Rigidbody))]
+public class Pullable : MonoBehaviour
+{
+    Rigidbody rb;
+    FixedJoint fixedJoint;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        fixedJoint = GetComponent<FixedJoint>();
+    }
+
+    public void Pull(Interactor source)
+    {
+        source.GetComponent<Bite>().IsBiting = true;
+        rb.isKinematic = false;
+        rb.useGravity = false;
+        fixedJoint.connectedBody = source.GetComponent<Rigidbody>();
+    }
+
+    public void StopPulling(Interactor source)
+    {
+        source.GetComponent<Bite>().IsBiting = false;
+        rb.isKinematic = true;
+        rb.useGravity = true;
+        fixedJoint.connectedBody = null;
+    }
+}
