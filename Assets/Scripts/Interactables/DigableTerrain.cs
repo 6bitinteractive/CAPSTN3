7044d12831e3f60d3f8324e0ae81273c39da3284
Meitantei-NepNep <nparuli@gmail.com>
2020-01-21 18:53:04 +0800

    Add DigableTerrain script

---
+++ Assets/Scripts/Interactables/DigableTerrain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigableTerrain : MonoBehaviour, IInteractable
+{
+    [SerializeField] private GameObject digableSpotToSpawn;
+    [SerializeField] private GameObject emptyDigableSpotToSpawn;
+    public void DisplayInteractability()
+    {
+       
+    }
+
+    public void Interact(Interactor source, IInteractable target)
+    {
+        Instantiate(digableSpotToSpawn, source.transform.position + source.transform.forward, Quaternion.identity);
+    }
+
+    public void ChangeDigableSpotToSpawn()
+    {
+        digableSpotToSpawn = emptyDigableSpotToSpawn;
+    }
+}
