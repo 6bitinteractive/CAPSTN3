159ada7e3107dc64ceed1a1902888dc640d6cf5b
Meitantei-NepNep <nparuli@gmail.com>
2020-01-22 08:33:31 +0800

    Add Talkable script

---
+++ Assets/Scripts/Interactables/Talkable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talkable : MonoBehaviour, IInteractable
+{
+    public void DisplayInteractability()
+    {
+      
+    }
+
+    public void Interact(Interactor source, IInteractable target)
+    {
+        //Call dialogue function here
+        Debug.Log(source + "Is talking to " + target);
+    }
+}
