1550367002605c6b084044a47b552e2b72d63e7c
Lim <michelle.lim@benilde.edu.ph>
2020-01-26 21:58:20 +0800

    Add DayProgression

---
+++ Assets/Scripts/DayProgression/DayProgression.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayProgression : MonoBehaviour
+{
+    // "Proper" count of day, starting with 1
+    public int CurrentDayCount => currentDay + 1;
+
+    private QuestManager questManager;
+
+    //[SerializeField]
+    private int currentDay;
+
+    private void Start()
+    {
+        questManager = SingletonManager.GetInstance<QuestManager>();
+        questManager.Initialize();
+
+        foreach (var quest in questManager.quests)
+        {
+            quest.OnQuestEnd.AddListener(EndDay); // Test
+        }
+
+        BeginDay();
+    }
+
+    public void BeginDay()
+    {
+        Debug.LogFormat("Beginning Day {0}", CurrentDayCount);
+        // Start the quest to be tackled for the day
+        questManager.ActivateQuest(currentDay);
+    }
+
+    public void EndDay()
+    {
+        Debug.LogFormat("Finished Day {0}", CurrentDayCount);
+        currentDay++;
+    }
+}
--- Assets/Scripts/Quest/QuestManager.cs
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -3,28 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// TODO: Let the DayProgressionManager handle ?
 public class QuestManager : Singleton<QuestManager>
 {
     [HideInInspector]
     public List<Quest> quests = new List<Quest>();
 
-    // Note: Dummy Data for testing
-    //private Quest quest = new Quest(); // Preferably, this (defining the quest details) should be done in the inspector
-
-    private void Start()
+    public void Initialize()
     {
         // Get all the quests
         quests.AddRange(GetComponentsInChildren<Quest>());
 
-        // Create the quest events for each quest
         foreach (var quest in quests)
-        {
             quest.Initialize();
+    }
 
-        }
-
-        // Start first quest
-        quests[0].ActivateQuest();
+    public void ActivateQuest(int index)
+    {
+        if (index < quests.Count)
+            quests[index].ActivateQuest();
     }
 }
