09f8c73cd933db7794f87a2c41458c7d02c07c7f
Lim <michelle.lim@benilde.edu.ph>
2020-01-28 18:05:47 +0800

    Add CurrentQuest property in QuestManager

--- Assets/Scripts/Quest/QuestManager.cs
+++ Assets/Scripts/Quest/QuestManager.cs
@@ -8,6 +8,8 @@ public class QuestManager : Singleton<QuestManager>
     [HideInInspector]
     public List<Quest> quests = new List<Quest>();
 
+    public Quest CurrentQuest { get; private set; }
+
     public void Initialize()
     {
         // Get all the quests
@@ -19,7 +21,12 @@ public class QuestManager : Singleton<QuestManager>
 
     public void ActivateQuest(int index)
     {
+        if (index < 0)
+            return;
+
         if (index < quests.Count)
             quests[index].ActivateQuest();
+
+        CurrentQuest = quests[index];
     }
 }
