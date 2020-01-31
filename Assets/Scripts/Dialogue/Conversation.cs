97a7662bec3b789001890f7541de291c22af2a2c
Lim <michelle.lim@benilde.edu.ph>
2020-01-31 12:37:03 +0800

    Add Dialogue related classes

---
+++ Assets/Scripts/Dialogue/Conversation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Conversation
+{
+    public Dialogue[] dialogue;
+
+    public void DisplayDialogueLine(int index)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueSpeaker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DialogueSpeaker : ScriptableObject
+{
+    [SerializeField] private Sprite image;
+    [SerializeField] private string speakerName;
+    [SerializeField] private string speakerDescription;
+
+    public Sprite Image => image;
+    public string DisplayName => speakerName;
+    public string Description => speakerDescription;
+}
