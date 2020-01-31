a84335ea9d4cdf875130a1a74461c8afc40adcfe
Meitantei-NepNep <nparuli@gmail.com>
2020-01-31 12:42:31 +0800

    Add GrayscaleFilter script, shader, material

---
+++ Assets/Shaders/GrayscaleFilter/GrayscaleFilter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GrayscaleFilter : MonoBehaviour
+{
+    public Material m_Material;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (m_Material != null)
+            Graphics.Blit(source, destination, m_Material);
+    }
+}
