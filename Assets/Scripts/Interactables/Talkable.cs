c2eb70ca07a5883a78df957655a5fbbf89cd9322
Lim <michelle.lim@benilde.edu.ph>
2020-02-04 11:48:42 +0800

    Comment out Debug statement in Talkable

--- Assets/Scripts/Interactables/Talkable.cs
+++ Assets/Scripts/Interactables/Talkable.cs
@@ -24,7 +24,7 @@ public class Talkable : MonoBehaviour, IInteractable
     public void Interact(Interactor source, IInteractable target)
     {
         //Call dialogue function here
-        Debug.Log(source + "Is talking to " + target);
+        //Debug.Log(source + "Is talking to " + target);
         OnTalk.Invoke();
         dialogueHandler.DisplayCurrentConversation();
     }
