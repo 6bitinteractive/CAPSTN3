9967340da895935830f60edfba9160e999f6799b
Lim <michelle.lim@benilde.edu.ph>
2020-02-05 20:06:54 +0800

    Add QuestEventStateCondition + prefab

--- Assets/Scripts/Objectives/Conditions/Condition.cs
+++ Assets/Scripts/Objectives/Conditions/Condition.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +7,9 @@ using UnityEngine.SceneManagement;
 
 public abstract class Condition : MonoBehaviour
 {
+    [Tooltip("Most likely for conditions used to listen for QuestEvent updates, not by QuestEvents themselves, e.g. for switching dialogue")]
+    [SerializeField] private bool activeAtStart;
+
     public Status CurrentStatus => currentStatus;
     public bool Satisfied { get; protected set; }
 
@@ -16,6 +19,12 @@ public abstract class Condition : MonoBehaviour
     protected static EventManager eventManager;
     private Status currentStatus;
     private bool initialized;
+
+    private void OnEnable()
+    {
+        if (activeAtStart && CurrentStatus != Status.Done)
+            SwitchStatus(Status.Active);
+    }
 
     public void SwitchStatus(Status status)
     {
---
+++ Assets/Scripts/Objectives/Conditions/Types/QuestEventStateCondition.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuestEventStateCondition : Condition
+{
+    [Header("Condition Requirements")]
+    [SerializeField] private GuidReference questEvent;
+    [SerializeField] private QuestEvent.Status requiredStatus = QuestEvent.Status.Active;
+
+    private QuestEvent requiredQuestEvent;
+    private QuestEvent.Status statusToAssess;
+
+    protected override void InitializeCondition()
+    {
+        base.InitializeCondition();
+        requiredQuestEvent = questEvent.gameObject.GetComponent<QuestEvent>();
+
+        // If the event has already passed
+        if (requiredQuestEvent.CurrentStatus != QuestEvent.Status.Inactive)
+        {
+            statusToAssess = requiredQuestEvent.CurrentStatus;
+            SwitchStatus(Status.Evaluating);
+        }
+        else
+        {
+            GetConditionRequirements();
+        }
+    }
+
+    protected override void FinalizeCondition()
+    {
+        base.FinalizeCondition();
+        switch (requiredStatus)
+        {
+            case QuestEvent.Status.Active:
+                requiredQuestEvent.OnActive.gameEvent.RemoveListener(OnQuestUpdate);
+                break;
+            case QuestEvent.Status.Done:
+                requiredQuestEvent.OnDone.gameEvent.RemoveListener(OnQuestUpdate);
+                break;
+        }
+    }
+
+    protected override bool IsSatisfied()
+    {
+        //Debug.Log("ASSESS: " + statusToAssess + " || " + requiredStatus);
+        return statusToAssess == requiredStatus;
+    }
+
+    protected override void OnSceneLoad(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        base.OnSceneLoad(scene, loadSceneMode);
+        GetConditionRequirements();
+    }
+
+    private void OnQuestUpdate(QuestEvent questEvent)
+    {
+        statusToAssess = questEvent.CurrentStatus;
+        SwitchStatus(Status.Evaluating);
+    }
+
+    private void GetConditionRequirements()
+    {
+        switch (requiredStatus)
+        {
+            case QuestEvent.Status.Inactive:
+                Debug.LogError("Please don't choose Inactive. Only Active and Done states are supported.");
+                break;
+            case QuestEvent.Status.Active:
+                requiredQuestEvent.OnActive.gameEvent.AddListener(OnQuestUpdate);
+                break;
+            case QuestEvent.Status.Done:
+                requiredQuestEvent.OnDone.gameEvent.AddListener(OnQuestUpdate);
+                break;
+        }
+    }
+}
