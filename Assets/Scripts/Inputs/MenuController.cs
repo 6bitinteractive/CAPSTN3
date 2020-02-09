97388149584c2f26acab9c70003d2b20a67bfb6d
Meitantei-NepNep <nparuli@gmail.com>
2020-02-09 17:53:22 +0800

    Add MenuController
    This is used to allow Gamepads/Controllers the ability to navigate UI

---
+++ Assets/Scripts/Inputs/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// This script's main purpose is to handle Menu navigation for the Gamepad/Controller
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private EventSystem eventSystem;
+    [SerializeField] private GameObject firstSelectedGameObject;
+
+    void Start()
+    {
+        eventSystem.SetSelectedGameObject(firstSelectedGameObject, null);
+    }
+}
