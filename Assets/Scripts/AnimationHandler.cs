5688e2e4f67b785a37eb4921760ccc37c5f0b6b9
Meitantei-NepNep <nparuli@gmail.com>
2020-02-09 12:02:38 +0800

    Add AnimationHandler Script
    A temporary way to turn player controller on/off i.e when dog performs dig action stop the dog from being able to move around

---
+++ Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationHandler : MonoBehaviour
+{
+    public UnityEvent OnAnimStart = new UnityEvent();
+    public UnityEvent OnAnimEnded = new UnityEvent();
+
+    public void StartAnimation()
+    {
+        OnAnimStart.Invoke();
+    }
+
+    public void EndAnimation()
+    {
+        OnAnimEnded.Invoke();
+    }
+}
