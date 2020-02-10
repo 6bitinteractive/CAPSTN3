e14090d66c5ececa3b1e9d07257d5756ee63ee5f
Lim <michelle.lim@benilde.edu.ph>
2020-02-11 00:32:16 +0800

    Add ConditionState listener

---
+++ Assets/Scripts/Objectives/Conditions/Types/ListeningForStatusChanges/ConditionState.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionState : Condition
+{
+    [SerializeField] private GuidReference requiredCondition;
+    [SerializeField] private Status requiredStatus = Status.Active;
+
+    private Condition reqCondition;
+    private Status statusToAssess;
+
+    protected override void InitializeCondition()
+    {
+        reqCondition = requiredCondition.gameObject.GetComponent<Condition>();
+        base.InitializeCondition();
+
+        // If the event has already passed
+        if (reqCondition.CurrentStatus != Status.Inactive || reqCondition.CurrentStatus != Status.Evaluating)
+        {
+            statusToAssess = reqCondition.CurrentStatus;
+            SwitchStatus(Status.Evaluating);
+        }
+        else
+        {
+            GetConditionRequirements();
+        }
+    }
+
+    protected override void FinalizeCondition()
+    {
+        base.FinalizeCondition();
+        switch (requiredStatus)
+        {
+            case Status.Active:
+                reqCondition.OnActive.gameEvent.RemoveListener(OnConditionUpdate);
+                break;
+            case Status.Done:
+                reqCondition.OnDone.gameEvent.RemoveListener(OnConditionUpdate);
+                break;
+        }
+    }
+
+    protected override bool IsSatisfied()
+    {
+        return statusToAssess == requiredStatus;
+    }
+
+    protected override void GetConditionRequirements()
+    {
+        base.GetConditionRequirements();
+        switch (requiredStatus)
+        {
+            case Status.Inactive:
+            case Status.Evaluating:
+                Debug.LogError("Please don't choose Inactive or Evaluating. Only Active and Done states are supported.");
+                break;
+            case Status.Active:
+                reqCondition.OnActive.gameEvent.AddListener(OnConditionUpdate);
+                break;
+            case Status.Done:
+                reqCondition.OnDone.gameEvent.AddListener(OnConditionUpdate);
+                break;
+        }
+    }
+
+    private void OnConditionUpdate(Condition condition)
+    {
+        statusToAssess = condition.CurrentStatus;
+        SwitchStatus(Status.Evaluating);
+    }
+}
