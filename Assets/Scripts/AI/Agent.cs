fd576f08104466f15bb9114b586d725c0f6d92f9
Meitantei-NepNep <nparuli@gmail.com>
2020-02-16 22:44:02 +0800

    Add Agent script

---
+++ Assets/Scripts/AI/Agent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+
+    public GameObject Target { get => target; set => target = value; }
+}
---
+++ Assets/Scripts/AI/StateMachine/States/State.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Agent))]
+public class State : MonoBehaviour
+{
+    protected Agent agent;
+    [SerializeField] private List<Transition> transitions = new List<Transition>();
+
+    void Awake()
+    {
+        agent = GetComponent<Agent>();
+    }
+
+    public virtual void OnEnable()
+    {
+
+    }
+
+    public virtual void OnDisable()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+        foreach (Transition transition in transitions)
+        {
+            if (transition.aiCondition.CheckCondition(agent))
+            {
+                transition.targetState.enabled = true; // Enabled component states
+                enabled = false; // Disable this state
+                return;
+            }
+        }
+    }
+
+    [Serializable]
+    public struct Transition
+    {
+        public AICondition aiCondition;
+        public State targetState;
+    }
+}
