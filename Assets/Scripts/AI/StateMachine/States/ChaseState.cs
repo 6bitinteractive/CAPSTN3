e3fbe5725cd9bfcdeb6876d16be5c160ad54acfb
Meitantei-NepNep <nparuli@gmail.com>
2020-02-16 22:44:44 +0800

    Add ChaseState script

---
+++ Assets/Scripts/AI/StateMachine/States/ChaseState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : State
+{
+    [SerializeField] private GameObject sightedIndicator;
+    [SerializeField] private float chaseSpeed = 2f;
+    [SerializeField] private float roationSpeed = 10f;
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        if (sightedIndicator)
+            sightedIndicator.SetActive(true);
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        if (sightedIndicator)
+            sightedIndicator.SetActive(false);
+    }
+
+    public override void Update()
+    {
+        if (agent.Target == null) return;
+        RotateTowardsTarget(agent.Target.transform);
+        transform.position = Vector3.MoveTowards(transform.position, agent.Target.transform.position, chaseSpeed * Time.deltaTime);
+    }
+
+    private void RotateTowardsTarget(Transform currentTarget)
+    {
+        Vector3 direction = (currentTarget.transform.position - transform.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, roationSpeed * Time.deltaTime);
+    }
+}
