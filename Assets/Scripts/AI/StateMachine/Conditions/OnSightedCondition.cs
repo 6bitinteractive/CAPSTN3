3de5d94895cf37d4e76c53e147ecdcdd53d985c9
Meitantei-NepNep <nparuli@gmail.com>
2020-02-16 22:45:57 +0800

    Add OnSightedCondition script

---
+++ Assets/Scripts/AI/StateMachine/Conditions/OnSightedCondition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "OnSightedCondition", menuName = "AI/Conditions/OnSightedCondition")]
+public class OnSightedCondition : AICondition
+{
+    public override bool CheckCondition(Agent agent)
+    {
+        return agent.Target;
+    }
+}
