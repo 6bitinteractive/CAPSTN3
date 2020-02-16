b620d39706c7ff947b996c0311dd87f499d1f93a
Meitantei-NepNep <nparuli@gmail.com>
2020-02-16 22:45:41 +0800

    Add OnLostSightCondition script

---
+++ Assets/Scripts/AI/StateMachine/Conditions/OnLostSightCondition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OnLostSightCondition", menuName = "AI/Conditions/OnLostSightCondition")]
+public class OnLostSightCondition : AICondition
+{
+    public override bool CheckCondition(Agent agent)
+    {
+        return agent.Target == null;
+    }
+}
