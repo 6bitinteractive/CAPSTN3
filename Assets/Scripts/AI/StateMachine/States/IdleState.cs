ede5ad06586dbac1801a205aefdb5f59e1d59cae
Meitantei-NepNep <nparuli@gmail.com>
2020-02-17 18:20:54 +0800

    Stop AI from moving when IdleState is enabled

--- Assets/Scripts/AI/StateMachine/States/IdleState.cs
+++ Assets/Scripts/AI/StateMachine/States/IdleState.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class IdleState : State
 {
     public override void OnEnable()
     {
         base.OnEnable();
+        agent.Target = null;
+        navMeshAgent.ResetPath(); // Stop moving
     }
 }
