326593cecc17150236556514924ceb4f7bfdbb9b
Lim <michelle.lim@benilde.edu.ph>
2020-02-23 07:51:46 +0800

    Fix some timing of the reactions;
    Fix position of correct diggable terrain

---
+++ Assets/Scripts/Objectives/Conditions/Controllers/ConditionStateController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Condition))]
+
+public class ConditionStateController : MonoBehaviour
+{
+    [SerializeField] private bool activeAtStart = true;
+
+    private Condition condition;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+
+        if (!activeAtStart)
+            yield break;
+
+        condition = GetComponent<Condition>();
+        condition.SwitchStatus(Condition.Status.Active);
+    }
+}
