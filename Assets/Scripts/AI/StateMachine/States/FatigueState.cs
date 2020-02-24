9d25d46cbc5b5f1bcd995b752099a36d44a66708
Meitantei-NepNep <nparuli@gmail.com>
2020-02-24 11:36:16 +0800

    Add FatigueState script

---
+++ Assets/Scripts/AI/StateMachine/States/FatigueState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatigueState : State
+{
+    [SerializeField] private Vision eyes;
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        agent.Target = null;
+        navMeshAgent.ResetPath(); // Stop moving
+        eyes.gameObject.SetActive(false);
+        Debug.Log("Fatigued");
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        eyes.gameObject.SetActive(true);
+        Debug.Log("No longer Fatigued");
+    }
+}
