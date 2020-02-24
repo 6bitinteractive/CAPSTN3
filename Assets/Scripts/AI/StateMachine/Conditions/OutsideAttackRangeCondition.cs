ebf1863907b77cb4172a7b3f669c03e4bdf34f7c
Meitantei-NepNep <nparuli@gmail.com>
2020-02-24 15:13:10 +0800

    Add attack scriptable object conditions

---
+++ Assets/Scripts/AI/StateMachine/Conditions/OutsideAttackRangeCondition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OutsideAttackRangeCondition", menuName = "AI/Conditions/OutsideAttackRangeCondition")]
+public class OutsideAttackRangeCondition : AICondition
+{
+    public override bool CheckCondition(Agent agent)
+    {
+        return agent.TargetWithinRange == null;
+    }
+}
---
+++ Assets/Scripts/AI/StateMachine/Conditions/WithinAttackRangeCondition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WithinAttackRangeCondition", menuName = "AI/Conditions/WithinAttackRangeCondition")]
+public class WithinAttackRangeCondition : AICondition
+{   public override bool CheckCondition(Agent agent)
+    {
+        return agent.TargetWithinRange;
+    }
+}
