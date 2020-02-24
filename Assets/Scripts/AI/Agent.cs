551d72a124678128abc597c2f855991240e7bdc2
Meitantei-NepNep <nparuli@gmail.com>
2020-02-24 15:12:38 +0800

    Add AttackRange script

--- Assets/Scripts/AI/Agent.cs
+++ Assets/Scripts/AI/Agent.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Agent : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private GameObject targetWithinRange;
 
     public GameObject Target { get => target; set => target = value; }
+    public GameObject TargetWithinRange { get => targetWithinRange; set => targetWithinRange = value; }
 }
---
+++ Assets/Scripts/AI/AttackRange.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackRange : MonoBehaviour
+{
+    [SerializeField] private LayerMask targetLayerMask;
+
+    public LayerMask TargetLayerMask { get => targetLayerMask; set => targetLayerMask = value; }
+    private Agent agent;
+    private SphereCollider sphereCollider;
+    private Attack attack;
+    private void Awake()
+    {
+        agent = GetComponentInParent<Agent>();
+        attack = GetComponentInParent<Attack>();
+        sphereCollider = GetComponent<SphereCollider>();
+        sphereCollider.radius = attack.AttackRadius;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        GameObject target = other.gameObject;
+        if (target == null) return;
+
+        int collisionLayerMask = 1 << other.gameObject.layer;
+
+        // If collides with target layer mask
+        if (collisionLayerMask == TargetLayerMask.value)
+        {
+            agent.TargetWithinRange = target;
+            return;
+        }
+        return;   
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (agent.TargetWithinRange != null && other.gameObject == agent.TargetWithinRange)
+            agent.TargetWithinRange = null;
+    }
+}
