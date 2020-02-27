0cc7da369e952798d718d6e53f1cb5fc64791d4a
Meitantei-NepNep <nparuli@gmail.com>
2020-02-27 17:16:23 +0800

    Add ShockState(WiP)

---
+++ Assets/Scripts/AI/StateMachine/States/ShockState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockState : State
+{
+    [SerializeField] private GameObject emoticon;
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        agent.Target = null;
+        navMeshAgent.ResetPath(); // Stop moving
+
+        if (emoticon)
+            emoticon.SetActive(true);
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        if (emoticon)
+            emoticon.SetActive(false);
+    }
+
+    public void Shock()
+    {
+        // Reminders for myself
+        // Look at target
+        // Play shock Animation
+    }
+}
