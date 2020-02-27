ac3da05419b2d544ed4eb0cf8104e4f3c34c27e0
Meitantei-NepNep <nparuli@gmail.com>
2020-02-27 17:16:44 +0800

    Add Despawnable script

---
+++ Assets/Scripts/Spawner/DeSpawnable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeSpawnable : MonoBehaviour
+{
+  public void Despawn()
+   {
+        gameObject.SetActive(false);
+   }
+}
