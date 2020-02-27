ee634909f58e34932281299ffcb70b8c065c28c1
Meitantei-NepNep <nparuli@gmail.com>
2020-02-27 17:15:05 +0800

    Add HeldObject script

---
+++ Assets/Scripts/AI/HeldObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeldObject : MonoBehaviour
+{
+    [SerializeField] private GameObject hand;
+    [SerializeField] private Pickupable heldObject;
+    [SerializeField] private Pickupable originalHeldObject;
+
+    public void OnDisable()
+    {
+        if (heldObject != null)
+            Destroy(heldObject.gameObject);
+
+        SpawnHeldObject();
+    }
+
+    public void DropHeldObject()
+    {
+        if (heldObject == null) return;
+        heldObject.DropObject(null);
+        heldObject = null;
+    }
+
+    public void SpawnHeldObject()
+    {
+        heldObject = Instantiate(originalHeldObject, hand.transform.position, Quaternion.identity);
+        heldObject.transform.parent = hand.transform;
+        heldObject.gameObject.SetActive(true);
+    }
+}
