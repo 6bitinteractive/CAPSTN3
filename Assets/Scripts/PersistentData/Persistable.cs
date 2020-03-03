24f1a99ee9dc414ddc55bdf0d37245fff9f2ca1a
Lim <michelle.lim@benilde.edu.ph>
2020-03-03 22:57:03 +0800

    Save Persistable's activeInHierarchy value by default

--- Assets/Scripts/PersistentData/Persistable.cs
+++ Assets/Scripts/PersistentData/Persistable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Let the object (with this component attached) save/load whatever it wants to save/load
-// In this case, by default the object simply saves values from the Transform component
+// In this case, by default the object simply saves values from the Transform component and if it is activeInHierarchy
 // Order matters! Save and Load should match; in this case, the order is position, rotation, then scale---i.e. we read in the same order as what we wrote.
 
 [DisallowMultipleComponent] // Make sure only one component per object is attached
@@ -15,6 +15,7 @@ public class Persistable : MonoBehaviour
         writer.Write(transform.localPosition);
         writer.Write(transform.localRotation);
         writer.Write(transform.localScale);
+        writer.Write(gameObject.activeInHierarchy);
     }
 
     public virtual void Load(GameDataReader reader)
@@ -22,6 +23,7 @@ public class Persistable : MonoBehaviour
         transform.localPosition = reader.ReadVector3();
         transform.localRotation = reader.ReadQuaternion();
         transform.localScale = reader.ReadVector3();
+        gameObject.SetActive(reader.ReadBool());
     }
 }
 
