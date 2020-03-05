846c14c1d3e901cce275878f853af997b56b3327
Meitantei-NepNep <nparuli@gmail.com>
2020-03-05 13:32:44 +0800

    Remade spawner logic

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] float spawnInterval = 3.5f;
-    [SerializeField] bool isSpawning = true;
-    [SerializeField] List<GameObject> pooledObjectList;
-    [SerializeField] List<GameObject> spawnPoints;
+    [SerializeField] protected float spawnInterval = 3.5f;
+    [SerializeField] protected bool isSpawning = true;
+    [SerializeField] protected List<GameObject> pooledObjectList;
+    [SerializeField] protected List<SpawnPoint> spawnPoints;
+    [SerializeField] protected List<SpawnPoint> availableSpawnPoints = new List<SpawnPoint>();
     private float timer;
+    private int randomSpawnPoint;
 
-    public void Start()
+    public virtual void Start()
     {
         for (int i = 0; i < pooledObjectList.Count; i++)
         {
@@ -18,7 +20,7 @@ public class Spawner : MonoBehaviour
         }
     }
 
-    public void Update()
+    public virtual void Update()
     {
         if (isSpawning == false) return;
 
@@ -31,17 +33,21 @@ public class Spawner : MonoBehaviour
         }
     }
 
-    public void Spawn()
+    public virtual void Spawn()
     {
-        int randomSpawnPoint = Random.Range(0, spawnPoints.Count);
+        availableSpawnPoints.Clear();
+        CheckSpawnPoints();
+
+        // If there are no available spawn points return
+        if (availableSpawnPoints.Count.Equals(0)) return;
 
         for (int i = 0; i < pooledObjectList.Count; i++)
-        {          
+        {
             // Check if there arent any active pooled objects
             if (pooledObjectList[i].activeInHierarchy == false)
             {
                 pooledObjectList[i].SetActive(true);
-                pooledObjectList[i].transform.position = spawnPoints[randomSpawnPoint].transform.position;
+                pooledObjectList[i].transform.position = availableSpawnPoints[randomSpawnPoint].transform.position;
                 pooledObjectList[i].transform.rotation = Quaternion.identity;
                 break;
             }
@@ -50,11 +56,26 @@ public class Spawner : MonoBehaviour
             else if (i == pooledObjectList.Count - 1)
             {
                 int randomSpawnablePooledObject = Random.Range(0, pooledObjectList.Count);
-                GameObject newPooledObject = Instantiate(pooledObjectList[randomSpawnablePooledObject], spawnPoints[randomSpawnPoint].transform.position, Quaternion.identity);
+                GameObject newPooledObject = Instantiate(pooledObjectList[randomSpawnablePooledObject], availableSpawnPoints[randomSpawnPoint].transform.position, Quaternion.identity);
                 newPooledObject.transform.parent = gameObject.transform;
                 newPooledObject.SetActive(false);
-                pooledObjectList.Add(newPooledObject);
+                pooledObjectList.Add(newPooledObject);                  
             }
         }
+            
+    }
+
+    private void CheckSpawnPoints()
+    {
+        for (int i = 0; i < spawnPoints.Count; i++)
+        {
+            // Check if there arent any active pooled objects
+            if (spawnPoints[i].CanSpawn == true)
+            {
+                availableSpawnPoints.Add(spawnPoints[i]);
+                randomSpawnPoint = Random.Range(0, availableSpawnPoints.Count);
+            }
+        }
+        return;
     }
 }
