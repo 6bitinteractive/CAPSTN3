316bb3a436ace6ba4ace2c699364bd35dee0e6c6
Meitantei-NepNep <nparuli@gmail.com>
2020-03-05 13:33:16 +0800

    Add SpawnPointCollisionCondition script

---
+++ Assets/Scripts/Spawner/SpawnPointCollisionCondition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointCollisionCondition : SpawnPoint
+{
+    [SerializeField] private LayerMask targetLayerMask;
+
+    public override void Start()
+    {
+        canSpawn = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        GameObject target = other.gameObject;
+        if (target == null) return;
+
+        int collisionLayerMask = 1 << target.layer;
+
+        // If collides with target layer mask
+        if (collisionLayerMask == targetLayerMask.value)
+        {
+            canSpawn = true;
+            return;
+        }
+        return;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canSpawn = false;
+    }
+}
