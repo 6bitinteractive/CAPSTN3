8c0fc0220bdbbb20a9aacd1fc2690640352b079a
Meitantei-NepNep <nparuli@gmail.com>
2020-03-05 13:32:59 +0800

    Add SpawnPoint script

---
+++ Assets/Scripts/Spawner/SpawnPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField] private protected bool canSpawn;
+
+    public bool CanSpawn { get => canSpawn; set => canSpawn = value; }
+
+    public virtual void Start()
+    {
+        canSpawn = true;
+    }
+}
