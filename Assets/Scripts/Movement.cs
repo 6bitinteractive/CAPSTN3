2ebbdbd22b3473cf26cf24d582b587d07bc23bcf
Meitantei-NepNep <nparuli@gmail.com>
2020-03-12 12:20:07 +0800

    Rewrite player movement to accommodate slopes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,45 +12,54 @@ public class Movement : MonoBehaviour
     [SerializeField] private float normalMultiplier = 1f;
     [SerializeField] private float hungryMultiplier = 0.5f;
 
+    [Header("Ground Check")]
+    [SerializeField] private float gravity = -30f;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float groundDistance = 0.4f;
+
     [Header("Other Fields")]
     [SerializeField] private Transform camera;
     [SerializeField] private Animator animator;
 
-    private Rigidbody rb;
     private float currentMultiplier = 1f;
+    private Vector3 currentDirection;
+    private bool isGrounded;
+    private CharacterController controller;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     public void Move(Vector3 direction, int modifier)
     {
-        direction = camera.TransformDirection(direction); // Face Camera
-        direction.y = 0f; // prevent player from moving vertically
-        direction.Normalize();
-        rb.MovePosition(rb.position + (direction * speed * currentMultiplier * modifier) * Time.deltaTime);
-        Rotate(direction);
+        currentDirection = direction;
+       
+        CheckIfGrounded();
+        GetDirection();
+        Rotate(currentDirection);
 
+        currentDirection.y += gravity * Time.deltaTime;
+        controller.Move((currentDirection * speed * currentMultiplier * modifier) * Time.deltaTime);
+      
         if (animator == null) return;
-        Animate(direction);
+        Animate(currentDirection);
     }
 
     public void Rotate(Vector3 direction)
     {
         if (direction != Vector3.zero)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(direction, rb.transform.up);
-            Quaternion newRotation = Quaternion.Lerp(rb.transform.rotation, targetRotation, rotationSpeed * currentMultiplier * Time.deltaTime);
-            rb.MoveRotation(newRotation);
+            Vector3 NextDir = new Vector3(direction.x, 0, direction.z);
+            transform.rotation = Quaternion.LookRotation(NextDir * rotationSpeed);
         }
-        else return;
     }
 
     public void Animate(Vector3 direction)
     {
         animator.SetFloat("VelX", direction.x);
-        animator.SetFloat("VelY", direction.y);
+      //  animator.SetFloat("VelY", direction.y);
     }
 
     public void UpdateMultiplier(Hunger hunger)
@@ -66,6 +75,25 @@ public class Movement : MonoBehaviour
             case Hunger.State.Hungry:
                 currentMultiplier = hungryMultiplier;
                 break;
+        }
+    }
+
+    private void GetDirection()
+    {
+        //Get Direction
+        currentDirection = camera.TransformDirection(currentDirection); // Face Camera
+        currentDirection.y = 0f;
+        currentDirection.Normalize();
+    }
+
+    private void CheckIfGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheckPos.position, groundDistance, layerMask);
+
+        if (isGrounded && currentDirection.y < 0)
+        {
+            currentDirection.y = -2f;
+            Debug.Log("grounded");
         }
     }
 }
