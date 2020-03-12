cba3af9c50e9c93b6658625b6e2dcc42cef77a8a
Meitantei-NepNep <nparuli@gmail.com>
2020-03-12 13:36:40 +0800

    Add TimerUI script

---
+++ Assets/Scripts/Timer/TimerUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TimerUI : MonoBehaviour
+{
+    public TextMeshProUGUI CountdownText;
+    private Timer timer;
+
+    void Start()
+    {
+        timer = GetComponent<Timer>();
+        CountdownText.text = timer.CurrentCountdownValue.ToString("F2");
+    }
+
+    void Update()
+    {
+        if (timer.CountdownValue >= 0f)
+            CountdownText.text = timer.CurrentCountdownValue.ToString("F2");
+
+    }
+}
