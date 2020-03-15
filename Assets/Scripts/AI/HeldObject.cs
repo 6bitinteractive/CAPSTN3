73e4d7dd38f0178c3fcee49c5f477027134b9b07
Meitantei-NepNep <nparuli@gmail.com>
2020-03-15 09:30:41 +0800

    Fix bug where HeldObjects were not resetting it's transform properly
    When a FoodHuman AI respawns the burger does not reset its transform properly

--- Assets/Scripts/AI/HeldObject.cs
+++ Assets/Scripts/AI/HeldObject.cs
@@ -8,6 +8,17 @@ public class HeldObject : MonoBehaviour
     [SerializeField] private Pickupable heldObject;
     [SerializeField] private Pickupable originalHeldObject;
 
+    private Vector3 originalScale;
+    private Vector3 originalPos;
+    private Quaternion originalRotation;
+    private Transform heldObjectTransform;
+    private void Start()
+    {
+        originalScale = originalHeldObject.transform.localScale;
+        originalPos = originalHeldObject.transform.localPosition;
+        originalRotation = originalHeldObject.transform.localRotation;
+    }
+
     public void OnDisable()
     {
         if (heldObject != null)
@@ -25,8 +36,18 @@ public class HeldObject : MonoBehaviour
 
     public void SpawnHeldObject()
     {
-        heldObject = Instantiate(originalHeldObject, hand.transform.position, Quaternion.identity);
-        heldObject.transform.parent = hand.transform;
+        heldObject = Instantiate(originalHeldObject, hand.transform.position, Quaternion.identity);     
+        ResetLocalTransform(heldObject);
+
         heldObject.gameObject.SetActive(true);
+    }
+
+    private void ResetLocalTransform(Pickupable heldObject)
+    {
+        heldObjectTransform = heldObject.transform;
+        heldObjectTransform.parent = hand.transform;
+        heldObjectTransform.localScale = originalScale;
+        heldObjectTransform.localPosition = originalPos;
+        heldObjectTransform.localRotation = originalRotation;
     }
 }
