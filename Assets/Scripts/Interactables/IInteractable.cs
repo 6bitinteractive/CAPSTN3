151ecfcd0c2bc7f9e1ceab9c6ee5ecae34a44325
Meitantei-NepNep <nparuli@gmail.com>
2020-03-16 10:58:30 +0800

    Add gameObject getter for IInteractable script

--- Assets/Scripts/Interactables/IInteractable.cs
+++ Assets/Scripts/Interactables/IInteractable.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public interface IInteractable
 {
+    GameObject gameObject { get; }
     void DisplayInteractability(); // Handles highlighting objects when nearby or displaying an icon to display interactability of an object
     void Interact(Interactor source, IInteractable target); // Handles interaction
 
