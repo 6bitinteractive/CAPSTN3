15eb0fc39a258045e16d065040bc2fd9ee71624e
Lim <michelle.lim@benilde.edu.ph>
2020-03-18 15:53:54 +0800

    Rename ListenerManager to ActiveAtSpecificDay;
    Move GameManager.cs to root Scripts folder
    Move ActiveAtSpecificDay.cs to Utils folder

---
+++ Assets/Scripts/Utils/ActiveAtSpecificDay.cs
@@ -0,0 +1,25 @@
+﻿using Meowfia.WanderDog;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// NOTE: This is not a good way to enable/disable quest-related objects
+// since those can be enabled/disabled as quests are updated within the day
+
+public class ActiveAtSpecificDay : MonoBehaviour
+{
+    [Tooltip("Not the index.")]
+    [SerializeField] private int day;
+
+    private static DayProgression dayProgression;
+
+    private void Start()
+    {
+        if (day <= 0)
+            Debug.LogError("Day cannot be 0 or negative");
+
+        dayProgression = dayProgression ?? SingletonManager.GetInstance<GameManager>().DayProgression;
+        //Debug.Log(dayProgression.CurrentDayCount);
+        gameObject.SetActive(day == dayProgression.CurrentDayCount);
+    }
+}
