dd90cbfad6dc74f987829d2f6dc3562bfb686cfa
tacodevourer <mariaangelica.canta@benilde.edu.ph>
2020-03-20 05:32:43 +0800

    Add Audio Manager scripts

---
+++ Assets/Scripts/Audio/PersistentAudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentAudioManager : Singleton<PersistentAudioManager>
+{
+    [SerializeField] private AudioSource bgm;
+    [SerializeField] private AudioSource ambient;
+
+    public AudioSource AudioSource(AudioType audioType)
+    {
+        switch (audioType)
+        {
+            case AudioType.BGM:
+                return bgm;
+            case AudioType.Ambient:
+                return ambient;
+        }
+
+        return null;
+    }
+
+    public void SwitchAudio(AudioSetting audioSetting, AudioType type)
+    {
+        AudioSource audioSource = AudioSource(type);
+        audioSource.clip = audioSetting.clip;
+        audioSource.volume = audioSetting.volume;
+        audioSource.Play();
+    }
+}
