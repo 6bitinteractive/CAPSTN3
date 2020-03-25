5e0e75fb84414a81f5bb2f361ff9ea9396d013cc
Lim <michelle.lim@benilde.edu.ph>
2020-03-25 11:33:34 +0800

    Refactor cross-scene delivery related scripts

---
+++ Assets/Scripts/CrossSceneCarry/CrossSceneObjectController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossSceneObjectController : MonoBehaviour
+{
+    [SerializeField] private GuidReference crossSceneObject;
+
+    private bool GuidReferenceAvailable => crossSceneObject.gameObject != null;
+
+    private CrossSceneObject crossScene;
+    private CrossSceneObject CrossSceneObject => crossScene ?? crossSceneObject.gameObject.GetComponent<CrossSceneObject>();
+
+    private Deliverable deliverable;
+    private Deliverable Deliverable
+    {
+        get { return deliverable ?? crossSceneObject.gameObject.GetComponent<Deliverable>(); }
+        set { deliverable = value; }
+    }
+
+    public void SetActive(bool value)
+    {
+        if (!GuidReferenceAvailable) return;
+        crossSceneObject.gameObject.SetActive(value);
+    }
+
+    public void SetParent(Transform transform)
+    {
+        if (!GuidReferenceAvailable) return;
+        crossSceneObject.gameObject.transform.SetParent(transform);
+    }
+
+    public void IsKinematic(bool value)
+    {
+        if (!GuidReferenceAvailable) return;
+
+        Rigidbody rb = GetComponentRequirement<Rigidbody>();
+        if (rb == null) return;
+
+        rb.isKinematic = value;
+    }
+
+    #region For Deliverables only
+    public void DeliverableEnable(bool updateData)
+    {
+        if (!GuidReferenceAvailable) return;
+
+        Deliverable = GetComponentRequirement<Deliverable>();
+        if (Deliverable == null) return;
+
+        Deliverable.Enable(updateData);
+    }
+
+    public void DeliverableDisable(bool updateData)
+    {
+        if (!GuidReferenceAvailable) return;
+
+        Deliverable = GetComponentRequirement<Deliverable>();
+        if (Deliverable == null) return;
+
+        Deliverable.Disable();
+    }
+
+    public void DeliverableActivate()
+    {
+        if (!GuidReferenceAvailable) return;
+
+        Deliverable = GetComponentRequirement<Deliverable>();
+        if (Deliverable == null) return;
+
+        Deliverable.Activate();
+    }
+    #endregion
+
+    private T GetComponentRequirement<T>()
+    {
+        T component = crossSceneObject.gameObject.GetComponent<T>();
+
+        //if (component == null)
+        //    Debug.LogErrorFormat("{0} does not have a {1} component", crossSceneObject.gameObject.name, typeof(T).ToString());
+
+        //Debug.LogFormat("{0} Has component {1}? {2}", crossSceneObject.gameObject.name, typeof(T).ToString(), "true");
+        return component;
+    }
+}
--- Assets/Scripts/Objectives/Conditions/Types/DeliverCondition/Mouth.cs
+++ Assets/Scripts/Objectives/Conditions/Types/DeliverCondition/Mouth.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class Mouth : Singleton<Mouth>
 {
-    private CrossSceneDeliverableHandler crossSceneDeliverableHandler;
-    private GameObject deliverableCarried;
-    private Deliverable deliverable;
+    private CrossSceneObjectHandler crossSceneDeliverableHandler;
+    private GameObject crossSceneGameObjectCarried;
+    private CrossSceneObject crossSceneComponent;
     private EventManager eventManager;
 
     private void Start()
     {
-        crossSceneDeliverableHandler = SingletonManager.GetInstance<CrossSceneDeliverableHandler>();
+        crossSceneDeliverableHandler = SingletonManager.GetInstance<CrossSceneObjectHandler>();
         eventManager = SingletonManager.GetInstance<EventManager>();
         eventManager.Subscribe<PickupEvent, PickupData>(CheckDeliverable);
         eventManager.Subscribe<EnterAreaEvent, EnterArea>(OnEnterArea);
@@ -20,11 +20,11 @@ public class Mouth : Singleton<Mouth>
 
     private void OnEnterArea(EnterArea enterArea)
     {
-        if (deliverable == null)
+        if (crossSceneComponent == null)
             return;
 
-        if (deliverable.IsCarried)
-            deliverable.MoveToPersistent();
+        if (crossSceneComponent.IsCarried)
+            crossSceneComponent.MoveToPersistentScene();
     }
 
     protected override void OnDestroy()
@@ -36,16 +36,12 @@ public class Mouth : Singleton<Mouth>
 
     private void CheckDeliverable(PickupData pickupData)
     {
-        deliverable = pickupData.pickupable.GetComponent<Deliverable>();
+        crossSceneComponent = pickupData.pickupable.GetComponent<CrossSceneObject>();
 
-        if (deliverable == null)
+        if (crossSceneComponent == null)
             return;
 
-        // We only care about cross-scene deliverables for now
-        if (!deliverable.IsCrossSceneDeliverable)
-            return;
-
-        deliverableCarried = pickupData.pickupable.gameObject;
-        crossSceneDeliverableHandler.DeliverableObj = deliverableCarried;
+        crossSceneGameObjectCarried = pickupData.pickupable.gameObject;
+        crossSceneDeliverableHandler.carriedObj = crossSceneGameObjectCarried;
     }
 }
